f5fce1730c46fcbeabdd9bb398e2eb6420b7cdc1
guille463 <guillejmo@gmail.com>
2026-07-10 09:30:56 +0200

    feat: hook para getAllBooks

---
+++ frontend/src/Hooks/useBooks.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+import { booksServices } from "../Services/booksServices";
+
+export function useBooks() {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const { data } = await booksServices.getAll();
+        setBooks(data);
+      } catch (err) {
+        setError(err.response?.data?.message ?? "Error searching books");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchBooks();
+  }, []);
+
+  return { books, loading, error };
+}

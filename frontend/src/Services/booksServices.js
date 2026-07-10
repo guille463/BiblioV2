bd3d2f471972a12b6766c091aeed0527cef6742a
guille463 <guillejmo@gmail.com>
2026-07-10 09:30:37 +0200

    feat: añadimos el service al frontend con sus correspondientes rutas

---
+++ frontend/src/Services/booksServices.js
@@ -0,0 +1,9 @@
+import { axiosClient } from "../Api/axiosClient";
+
+export const booksServices = {
+  getAll: () => axiosClient.get("/books"),
+  getById: (id) => axiosClient.get(`/books/${id}`),
+  createBook: (data) => axiosClient.post("/books", data),
+  updateBook: (id, data) => axiosClient.put(`/books/${id}`, data),
+  removeBook: (id) => axiosClient.delete(`/books/${id}`),
+};

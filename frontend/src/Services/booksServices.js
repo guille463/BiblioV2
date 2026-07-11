91a63544bba0655a1d3f5f3f00f86712a56f8aa6
guille463 <guillejmo@gmail.com>
2026-07-11 11:45:46 +0200

    feat: añado barra de busqieda

--- frontend/src/Services/booksServices.js
+++ frontend/src/Services/booksServices.js
@@ -3,6 +3,7 @@ import axiosClient from "../Api/axiosClient";
 export const booksServices = {
   getAll: () => axiosClient.get("/books"),
   getById: (id) => axiosClient.get(`/books/${id}`),
+  getBookByName: (title) => axiosClient.get(`/books/title/${title}`),
   createBook: (data) => axiosClient.post("/books", data),
   updateBook: (id, data) => axiosClient.put(`/books/${id}`, data),
   removeBook: (id) => axiosClient.delete(`/books/${id}`),

4ae5a9a639fa7f5129e48cf65d0fe0cda72499f5
guille463 <guillejmo@gmail.com>
2026-07-15 14:12:02 +0200

    fix: estado de los botones y añadir libros favoritos a array

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Header } from "./Components/Header";
 import { BooksPage } from "./Pages/BookPage";
+import { FavBookPage } from "./Pages/FavBookPage";
 
 function App() {
   return (
     <>
       <Header />
-      <BooksPage />
+      <Routes>
+        <Route path="/books" element={<BooksPage />} />
+        <Route path="/books/favs" element={<FavBookPage />} />
+      </Routes>
     </>
   );
 }
--- frontend/src/Components/BookCard.jsx
+++ frontend/src/Components/BookCard.jsx
@@ -1,39 +1,30 @@
-import { useState } from "react";
 import "./BookCard.css";
 
-export function BookCard({ title, author, isbn, genre, year, stock }) {
-  const isAvailable = stock > 0;
-  const [isLiked, setLiked] = useState(false);
+export function BookCard({ book, isFav, onToggleFav }) {
+  const isAvailable = book.stock > 0;
   const textAvailable = isAvailable ? "Disponible" : "No Disponible";
-  const textLiked = isLiked ? "Me gusta" : "Añadir a mi Lista";
-  const classNameLikedButton = isLiked ? "likedBook" : "notLikedBook";
-
-  const handleClick = () => {
-    setLiked(!isLiked);
-  };
+  const favText = isFav ? "Me gusta" : "Añadir a mi lista";
 
   return (
     <article className="book-card">
       <header className="book-card-header">
         <img
           className="book-card-img"
-          src={`https://covers.openlibrary.org/b/isbn/${isbn}-M.jpg`}
-          alt={title}
+          src={`https://covers.openlibrary.org/b/isbn/${book.isbn}-M.jpg`}
+          alt={book.title}
         />
         <div className="book-card-info">
-          <h3 className="book-card-title">Titulo: {title}</h3>
-          <p className="book-card-author">Autor: {author}</p>
-          <p className="book-card-isbn">ISBN: {isbn}</p>
-          <p className="book-card-genre">Genero: {genre}</p>
-          <p className="book-card-year">Año: {year}</p>
-          <p className="book-card-stock">Stock: {stock}</p>
+          <h3 className="book-card-title">Titulo: {book.title}</h3>
+          <p className="book-card-author">Autor: {book.author}</p>
+          <p className="book-card-isbn">ISBN: {book.isbn}</p>
+          <p className="book-card-genre">Genero: {book.genre}</p>
+          <p className="book-card-year">Año: {book.year}</p>
+          <p className="book-card-stock">Stock: {book.stock}</p>
           <p className="book-card-stock-info">Estado: {textAvailable}</p>
         </div>
       </header>
       <aside>
-        <button className={classNameLikedButton} onClick={handleClick}>
-          {textLiked}
-        </button>
+        <button onClick={() => onToggleFav(book)}>{favText}</button>
       </aside>
     </article>
   );

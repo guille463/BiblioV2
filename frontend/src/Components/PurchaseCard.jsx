6d80a7efeea74b462e29cfca81d9cef252b628f5
guille463 <guillejmo@gmail.com>
2026-07-18 19:15:47 +0200

    fix: lista del carrito

--- frontend/src/Components/PurchaseCard.jsx
+++ frontend/src/Components/PurchaseCard.jsx
@@ -1,20 +1,20 @@
-import { FavEmoji } from "../utils/Emojis";
+import "./PurchaseCard.css";
 
-export function PurchaseCard({ book, isFav }) {
+export function PurchaseCard({ book, onRemove }) {
   return (
     <article className="purchase-card">
-      <header className="purchase-card-header">
-        <img
-          className="purchase-card-img"
-          src={`https://covers.openlibrary.org/b/isbn/${book.isbn}-M.jpg`}
-          alt={book.title}
-        />
-        <div className="purchase-card-info">
-          <h3 className="purchase-card-title">Titulo: {book.title}</h3>
-          <p className="purchase-card-isbn">ISBN: {book.isbn}</p>
-          {isFav && <p className="purchase-card-fav">{FavEmoji}</p>}
-        </div>
-      </header>
+      <img
+        className="purchase-card-img"
+        src={`https://covers.openlibrary.org/b/isbn/${book.isbn}-S.jpg`}
+        alt={book.title}
+      />
+      <div className="purchase-card-info">
+        <p className="purchase-card-title">{book.title}</p>
+        <p className="purchase-card-isbn">{book.isbn}</p>
+      </div>
+      <button className="purchase-card-remove" onClick={() => onRemove(book)}>
+        Quitar
+      </button>
     </article>
   );
 }
